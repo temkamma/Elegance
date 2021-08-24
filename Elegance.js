c8e117d863e4b4e5e2b462fdcd7d81e941af31e8
temkamma <82313739+temkamma@users.noreply.github.com>
2021-08-24 13:18:59 +0300

    Delete Elegance.js

--- Elegance.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.burger').onclick = function() {
-  document.querySelector('.burger').classList.toggle('burger-active');
-  document.querySelector('.header-menu').classList.toggle('header-menu-active');
-  $('body').toggleClass('lock');
-}
